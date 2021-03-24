d8cdec962ae791f83fae5d2e2a3a389a59a6fe45
Will Custode <wjcustode@gmail.com>
2021-03-24 07:46:55 -0400

    Added redirection for home

--- Blog/Blog.Web/Startup.cs
+++ Blog/Blog.Web/Startup.cs
@@ -1,16 +1,45 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog.Web
 {
+    public class DefaultController : Controller
+    {
+        [HttpGet, Route("")]
+        public IActionResult RedirectHome()
+        {
+            return Redirect("index.html");
+        }
+    }
+
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseEndpoints(e =>
+            {
+                e.MapControllers();
+            });
         }
     }
 }
