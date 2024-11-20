e575b9cf29771800bfb07d41804ac345fca0630c
Will Custode <wjcustode@gmail.com>
2024-11-20 12:07:57 -0500

    Updated

--- Blog/Blog.Generator/obj/Debug/net6.0/Blog.Generator.AssemblyInfo.cs
+++ Blog/Blog.Generator/obj/Debug/net6.0/Blog.Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0f690d1643aafda059963ccb7975d6fed106f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
